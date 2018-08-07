52b06c2217729d195331b9d7228a77ee2f853338
Skyler Escobedo <skylerescobedo@gmail.com>
2018-08-06 22:36:27 -0700

    Updated Styles, Clear Input Text on SignUp

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, ImageBackground, Alert } from "react-native";
 import { Button, Text, Form, Item, Input, Label, Icon } from "native-base";
 import { NavigationActions } from "react-navigation";
 import * as firebase from "firebase";
-const remote = "https://wallpapercave.com/wp/12Pyika.jpg";
+const remote = "https://i.imgur.com/wNtUql0.png";
 
 class Signup extends React.Component {
   state = {
@@ -29,6 +29,7 @@ class Signup extends React.Component {
         .then(
           () => {
             this.props.navigation.navigate("Main");
+            return this.setState({ email: "", password: "", passwordConfirm: ""});
           },
           error => {
             Alert.alert(error.message);
@@ -40,6 +41,7 @@ class Signup extends React.Component {
   };
 
   render() {
+    const { email, password, passwordConfirm } = this.state;
     return (
       <ImageBackground style={styles.flex} source={{ uri: remote }}>
         <Form style={styles.container}>
@@ -48,6 +50,7 @@ class Signup extends React.Component {
             <Icon style={styles.color} name="person" />
             <Label style={styles.color}>Email</Label>
             <Input
+              value={email}
               style={{ color: "#fff" }}
               autoCorrect={false}
               autoCapitalize="none"
@@ -59,6 +62,7 @@ class Signup extends React.Component {
             <Icon style={styles.color} name="lock" />
             <Label style={styles.color}>Password</Label>
             <Input
+              value={password}
               style={{ color: "#fff" }}
               secureTextEntry={true}
               autoCorrect={false}
@@ -71,6 +75,7 @@ class Signup extends React.Component {
             <Icon style={styles.color} name="lock" />
             <Label style={styles.color}>Retype Password</Label>
             <Input
+              value={passwordConfirm}
               style={{ color: "#fff" }}
               secureTextEntry={true}
               autoCorrect={false}
@@ -80,9 +85,17 @@ class Signup extends React.Component {
           </Item>
           {/* Submit Button */}
           <Button
-            style={{ marginTop: 20 }}
-            full
+            style={{
+              marginTop: 20,
+              height: 50,
+              width: "50%",
+              alignSelf: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.3)"
+            }}
+            light
             rounded
+            bordered
             onPress={() => this.signUpUser(this.state.email, this.state.password)}
           >
             <Text style={{ color: "white", fontSize: 16 }}>Sign up</Text>
